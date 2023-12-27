79dc0e35c3c507db47c376ce44cb26c9e6b565e4
taiyou <hiromacha1116@icloud.com>
2023-12-28 00:39:08 +0900

    postsをPostsで管理

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,10 @@
-import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Posts from "./components/Posts";
-import { fetchPosts } from "./api/postApi";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PostPage from "./components/PostPage";
 
-interface Post {
-  id: number,
-  user_id: number,
-  title: string,
-  body: string,
-  username: string,
-}
-
-interface Response {
-  posts: Post[]
-}
-
 function App() {
-
-  const [posts, setPosts] = useState<Post[]>([]);
-
-  useEffect(() => {
-    const getPosts = async () => {
-      try {
-        const response: Response = await fetchPosts();
-        setPosts(response.posts);
-      } catch(error) {
-        console.error('Error fetching users:', error);
-      }
-    }
-    getPosts();
-  }, [])
-
+  
   return (
     <Router>
       <div>
@@ -40,7 +12,7 @@ function App() {
       </div>
       <Routes>
         <Route path="/newpost" element={<PostPage />} />
-        <Route path="/" element={<Posts posts={posts} />} />
+        <Route path="/" element={<Posts />} />
       </Routes>
     </Router>
   );
--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,17 +1,42 @@
 
-interface Response {
-  posts: any,
+import { useEffect, useState } from "react";
+import { fetchPosts } from "../api/postApi";
+
+interface Post {
+  id: number,
+  user_id: number,
+  title: string,
+  body: string,
+  username: string,
 }
 
-function Posts(p: Response) {
-  const { posts } = p;
+interface Response {
+  posts: Post[]
+}
+
+
+function Posts() {
+
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const response: Response = await fetchPosts();
+        setPosts(response.posts);
+      } catch(error) {
+        console.error('Error fetching users:', error);
+      }
+    }
+    getPosts();
+  }, [])
 
   return (
     <div>
       {/* 配列であるpostsの username を受け取る */}
       { posts?.map((post: any) => (
-                <div> { post.title + " : " + post.username } </div>
-              )) }
+        <div> { post.title + " : " + post.username } </div>
+      )) }
     </div>
   )
 }
