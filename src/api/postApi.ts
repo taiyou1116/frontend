9ec57409ede57493d64ca4747a64bf82d6817931
taiyou <hiromacha1116@icloud.com>
2023-12-29 00:14:19 +0900

    データ挿入(テスト前)

--- src/api/postApi.ts
+++ src/api/postApi.ts
@@ -11,3 +11,23 @@ export const fetchPosts = async () => {
         throw error;
     }
 }
+
+export const submitPost = async (title: string, content: string) => {
+    try {
+        const response = await fetch('http://localhost:8000/api/submit-post', {
+            method: 'POST',
+            body: JSON.stringify({ title, content }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('サーバーからのレスポンスが正常ではありません');
+        }
+        const responseData = await response.json();
+        console.log('レスポンス: ', responseData);
+    } catch(error) {
+        console.error('エラーが発生しました:', error);
+    }
+}
--- src/components/PostPage.tsx
+++ src/components/PostPage.tsx
@@ -1,13 +1,32 @@
+// 新しい投稿を作成するページ
+
+import { useState } from "react";
+import { submitPost } from "../api/postApi";
 
 function PostPage() {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    
+    submitPost(title, content);
+    console.log('送信されたデータ:', { title, content });
+  };
+
   return (
-    <div className=" h-screen flex flex-col items-center justify-center">
-      <div className=" bg-slate-400 p-5 flex flex-col gap-5 w-3/5 h-5/6 items-center justify-center">
-        <h1>新しい投稿</h1>
-        <input placeholder="タイトル" className=" w-3/5 title-height"/>
-        <textarea placeholder="投稿内容" className=" w-3/5 h-3/5 resize-none"/>
+    <form onSubmit={handleSubmit}>
+      <div className=" h-screen flex flex-col items-center justify-center">
+        <div className=" bg-slate-400 p-5 flex flex-col gap-5 w-3/5 h-5/6 items-center justify-center">
+          <h1>新しい投稿</h1>
+          <input placeholder="タイトル" className=" w-3/5 title-height" onChange={(e) => setTitle(e.target.value)}/>
+          <textarea placeholder="投稿内容" className=" w-3/5 h-3/5 resize-none" onChange={(e) => setContent(e.target.value)}/>
+          <button className=" bg-white py-1 px-4" type="submit">
+            投稿
+          </button>
+        </div>
       </div>
-    </div>
+    </form>
   )
 }
 
