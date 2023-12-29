aad444d3b0ed784aaab6c41567f1e68a99089e8f
taiyou <hiromacha1116@icloud.com>
2023-12-29 23:25:41 +0900

    username, title, bodyの表示

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,11 +1,17 @@
 
-function Post() {
+interface PostElement {
+  title: string,
+  body: string,
+  username: string,
+}
+
+function Post(post: PostElement) {
+  const { title, body, username } = post;
   return (
-    <div>
-      <title>タイトル</title>
-      <div>
-        ボディ
-      </div>
+    <div className=" bg-slate-400 flex flex-col p-5 gap-3 w-11/12">
+      <div>{ username }</div>
+      <div>{ title }</div>
+      <div>{ body }</div>
     </div>
   )
 }
--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,8 +1,13 @@
 
 import { useEffect, useState } from "react";
 import { fetchPosts } from "../api/postApi";
+import Post from "./Post";
 
-interface Post {
+interface Response {
+  posts: PostElement[]
+}
+
+interface PostElement {
   id: number,
   user_id: number,
   title: string,
@@ -10,14 +15,9 @@ interface Post {
   username: string,
 }
 
-interface Response {
-  posts: Post[]
-}
-
-
 function Posts() {
 
-  const [posts, setPosts] = useState<Post[]>([]);
+  const [posts, setPosts] = useState<PostElement[]>([]);
 
   useEffect(() => {
     const getPosts = async () => {
@@ -32,10 +32,14 @@ function Posts() {
   }, [])
 
   return (
-    <div>
-      ホーム画面
-      { posts?.map((post: any) => (
-        <div key={post.id}> { post.title + " : " + post.username } </div>
+    <div className=" flex flex-col gap-3 items-center justify-start my-5 h-screen w-screen">
+      { posts?.map((post: PostElement) => (
+          <Post 
+            key={post.id}
+            title={post.title}
+            body={ post.body }
+            username={ post.username }
+          />
       )) }
     </div>
   )
